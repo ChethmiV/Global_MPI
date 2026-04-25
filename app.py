9bf29c279680b129a7a174a59ee6c17f5dfc191d
Chethmi Narayanage <chethmi27narayanage@gmail.com>
2026-04-25 17:42:12 +0530

    Cleaned data set created and images added

--- app.py
+++ app.py
@@ -2,5 +2,16 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.title("Global MPI Dashboard 🌍")
-st.write("Let’s analyze global poverty data")
+# --- Configuration ---
+st.set_page_config(page_title="Global MPI Dashboard", page_icon="🌍", layout="wide")
+
+# --- Title and Introduction ---
+st.markdown("<h1 style='text-align: center;'>Global Multidimensional Poverty Index (MPI)</h1>", unsafe_allow_html=True)
+st.image("2.png", use_container_width=True)
+st.markdown("""
+<p font-size: 18px;'>
+<strong>Understanding global poverty patterns and identifying high-risk regions and drivers.</strong><br>
+Designed for the Global Conference on Sustainability to provide policymakers and finance professionals with actionable insights.<br>
+<em>Aligns with UN Sustainable Development Goal (SDG) 1: End poverty in all its forms everywhere.</em>
+</p>
+""", unsafe_allow_html=True)
